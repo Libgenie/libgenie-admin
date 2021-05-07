eb65f346b856a4866178682657f9ba9371d89316
saby22 <sabyasachi.tffs@gmail.com>
2021-05-08 02:23:07 +0530

    Fetched request collection from firebase and sorted it according to their status

--- store/reducers/requestReducer.js
+++ store/reducers/requestReducer.js
@@ -1,11 +1,30 @@
 import {STORE_REQUESTS, CLEAR_REQUESTS} from '../constants';
 
-const initialState = [];
+const initialState = {pending: [], ready: [], issued: []};
 
 const requestReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case STORE_REQUESTS:
-			return action.payload;
+			const pending = [];
+			const ready = [];
+			const issued = [];
+			action.payload.forEach(request => {
+				switch (request.status) {
+					case 3:
+						pending.push(request);
+						break;
+
+					case 2:
+						ready.push(request);
+						break;
+
+					case 1:
+						issued.push(request);
+						break;
+				}
+			});
+
+			return {pending, ready, issued};
 		case CLEAR_REQUESTS:
 			return initialState;
 
