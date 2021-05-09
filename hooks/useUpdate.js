5e4fdbbc05dc73d4c2c0fddf25749846debf338f
saby22 <sabyasachi.tffs@gmail.com>
2021-05-09 21:41:48 +0530

    Fixed useUpdate Hook

--- hooks/useUpdate.js
+++ hooks/useUpdate.js
@@ -1,20 +1,33 @@
 import {useState} from 'react';
 import axios from 'axios';
 const url = 'https://libgenie.netlify.app/.netlify/functions/update';
-const useUpdate = (issue_id, return_date = 'undefined') => {
+const useUpdate = () => {
 	const [loading, setIsLoading] = useState(false);
 	const [error, setError] = useState(false);
 	const [errorMessage, setErrorMessage] = useState('');
-	const config = {
-		method: 'POST',
-		baseURL: url,
-		data: {
-			issue_id,
-			return_date,
-		},
+	const [isClicked, setIsClicked] = useState({
+		issue_id: null,
+		return_date: null,
+	});
+
+	useEffect(() => {
+		if (isClicked.issue_id)
+			createConfig(isClicked.issue_id, isClicked.return_date);
+	}, [isClicked]);
+
+	const createConfig = (issue_id, return_date) => {
+		const config = {
+			method: 'POST',
+			baseURL: url,
+			data: {
+				issue_id,
+				return_date,
+			},
+		};
+		makeUpdate(config);
 	};
 
-	const makeUpdate = async () => {
+	const makeUpdate = async config => {
 		try {
 			setIsLoading(true);
 			const response = await axios(config);
@@ -26,15 +39,15 @@ const useUpdate = (issue_id, return_date = 'undefined') => {
 		} finally {
 			setIsLoading(false);
 			setError(false);
-			return {
-				loading,
-				error,
-				errorMessage,
-			};
 		}
 	};
 
-	makeUpdate();
+	return {
+		loading,
+		error,
+		errorMessage,
+		setIsClicked,
+	};
 };
 
 export default useUpdate;
