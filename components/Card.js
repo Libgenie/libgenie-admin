ad7dd169d3f9ec5d325c9f89ba7fc720a061117a
saby22 <sabyasachi.tffs@gmail.com>
2021-05-13 00:39:30 +0530

    Fetched Student Details for individual components

--- components/Card.js
+++ components/Card.js
@@ -1,134 +1,200 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import useUpdate from '../hooks/useUpdate';
-import { View, Text, Alert, TouchableOpacity } from 'react-native';
+import {
+	View,
+	Text,
+	Alert,
+	TouchableOpacity,
+	ActivityIndicator,
+} from 'react-native';
 import styles from '../screens/pendingstyles';
+import * as firebase from 'firebase';
 // import Returned from '../assets/Returned';
 
-const Card = ({ item }) => {
-  const { loading, error, errorMessage, setIsClicked, setError } = useUpdate();
-  let btnColor = '';
-  let btn = '';
-  if (item.status === 3) {
-    btnColor = '#f9a826';
-  } else if (item.status === 2) {
-    btnColor = '#00B7FF';
-  } else if (item.status === 1) {
-    if (new Date(item.return_date) < new Date()) {
-      btnColor = '#c90411';
-      btn = 'Running Late';
-    } else {
-      btnColor = '#33af85';
-      btn = 'Accept Return';
-    }
-  }
+const CardText = ({header, value, color}) => {
+	return (
+		<View style={styles.dateContainer}>
+			<View
+				style={{
+					width: 10,
+					height: 10,
+					borderRadius: 10,
+					marginHorizontal: 5,
+					backgroundColor: color,
+					opacity: 0.6,
+				}}
+			/>
+			<Text style={styles.date}>{`${header}: ${value}`}</Text>
+		</View>
+	);
+};
 
-  useEffect(() => {
-    if (error) {
-      Alert.alert('Error!', errorMessage, [
-        {
-          text: 'Ok',
-          onPress: () => {
-            console.log('Alerted!');
-            setError(false);
-          },
-        },
-      ]);
-    }
-  }, [error]);
+const Card = ({item}) => {
+	const {loading, error, errorMessage, setIsClicked, setError} = useUpdate();
+	const [student, setStudent] = useState(null);
+	let btnColor = '';
+	let btn = '';
+	if (item.status === 3) {
+		btnColor = '#f9a826';
+	} else if (item.status === 2) {
+		btnColor = '#00B7FF';
+	} else if (item.status === 1) {
+		if (new Date(item.return_date) < new Date()) {
+			btnColor = '#c90411';
+			btn = 'Running Late';
+		} else {
+			btnColor = '#33af85';
+			btn = 'Accept Return';
+		}
+	}
 
-  const handleClick = () => {
-    console.log('Clicked!');
-    setIsClicked({ issue_id: item.request_id, return_date: '2021-05-10T18:29:59.000Z' });
-  };
-  return (
-    <View style={styles.subCardContainer}>
-      <View style={{ width: '100%' }}>
-        <Text style={styles.bookName} numberOfLines={1}>
-          {item.name.charAt(0).toUpperCase() + item.name.substring(1)}
-        </Text>
+	useEffect(() => {
+		if (error) {
+			Alert.alert('Error!', errorMessage, [
+				{
+					text: 'Ok',
+					onPress: () => {
+						console.log('Alerted!');
+						setError(false);
+					},
+				},
+			]);
+		}
+	}, [error]);
 
-        <View style={styles.dateContainer}>
-          <View
-            style={{
-              width: 10,
-              height: 10,
-              borderRadius: 10,
-              marginHorizontal: 5,
-              backgroundColor: '#33af85',
-              opacity: 0.6,
-            }}></View>
-          <Text style={styles.date}>{`Author : ${item.author}`}</Text>
-        </View>
-        <View style={styles.dateContainer}>
-          <View
-            style={{
-              width: 10,
-              height: 10,
-              borderRadius: 10,
-              marginHorizontal: 5,
-              backgroundColor: 'magenta',
-              opacity: 0.6,
-            }}></View>
-          <Text style={styles.date}>{`Edition : ${item.edition}`}</Text>
-        </View>
-        <View style={styles.dateContainer}>
-          <View
-            style={{
-              width: 10,
-              height: 10,
-              borderRadius: 10,
-              marginHorizontal: 5,
-              backgroundColor: 'orange',
-              opacity: 0.6,
-            }}></View>
-          <Text style={styles.date}>{`Requested by: ${item.stuid}`}</Text>
-        </View>
-        <View
-          style={{
-            width: '100%',
-            borderWidth: 1,
-            marginVertical: 20,
-            opacity: 0.2,
-          }}></View>
-        <View>
-          <TouchableOpacity onPress={handleClick} disabled={loading} activeOpacity={0.6}>
-            <View
-              style={{
-                marginBottom: 10,
-                backgroundColor: btnColor,
-                borderRadius: 12,
-                padding: 12,
-                elevation: 6,
-              }}>
-              {!loading ? (
-                <Text
-                  style={{
-                    textAlign: 'center',
-                    fontSize: 17,
-                    color: 'white',
-                    fontFamily: 'Lato_400Regular',
-                    letterSpacing: 2,
-                  }}>
-                  {item.status === 3 ? 'Accept Request' : item.status === 2 ? 'Update Status' : btn}
-                </Text>
-              ) : (
-                <Text
-                  style={{
-                    textAlign: 'center',
-                    fontSize: 13,
-                    color: 'white',
-                    fontFamily: 'Lato_400Regular',
-                    letterSpacing: 2,
-                  }}>
-                  {item.status === 2 ? 'Updating...' : 'Accepting...'}
-                </Text>
-              )}
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
-  );
+	useEffect(() => {
+		const fetchStudent = async () => {
+			const db = firebase.firestore();
+			const studentDoc = await db
+				.collection('users')
+				.doc(item.student_uid)
+				.get();
+			setTimeout(() => {
+				setStudent(studentDoc.data());
+			}, 1000);
+		};
+		fetchStudent();
+	}, []);
+
+	const handleClick = () => {
+		console.log('Clicked!');
+		setIsClicked({
+			issue_id: item.request_id,
+			return_date: '2021-05-15T18:29:59.000Z',
+		});
+	};
+	return (
+		<View style={styles.subCardContainer}>
+			<View style={{width: '100%'}}>
+				<Text style={styles.bookName} numberOfLines={1}>
+					{item.name.charAt(0).toUpperCase() + item.name.substring(1)}
+				</Text>
+
+				<CardText header='Author' value={item.author} color={btnColor} />
+				<CardText header='Edition' value={item.edition} color={btnColor} />
+
+				{item.pickup_date && (
+					<CardText
+						header='Pickup Date'
+						value={new Date(item.pickup_date).toDateString()}
+						color={btnColor}
+					/>
+				)}
+				{item.return_date && (
+					<CardText
+						header='Return Date'
+						value={new Date(item.return_date).toDateString()}
+						color={btnColor}
+					/>
+				)}
+
+				{!student ? (
+					<View>
+						<ActivityIndicator
+							size='large'
+							color={
+								item.status === 3
+									? '#f9a826'
+									: item.status === 2
+									? '#00B7FF'
+									: '#33af85'
+							}
+						/>
+					</View>
+				) : (
+					<View>
+						<CardText
+							header='College Id'
+							value={student.college_id}
+							color={btnColor}
+						/>
+						<CardText
+							header='Name'
+							value={student.display_name}
+							color={btnColor}
+						/>
+						<CardText header='Stream' value={student.stream} color={btnColor} />
+						<CardText
+							header='Semester'
+							value={student.semester}
+							color={btnColor}
+						/>
+					</View>
+				)}
+
+				<View
+					style={{
+						width: '100%',
+						borderWidth: 1,
+						marginVertical: 20,
+						opacity: 0.2,
+					}}></View>
+				<View>
+					<TouchableOpacity
+						onPress={handleClick}
+						disabled={loading}
+						activeOpacity={0.6}>
+						<View
+							style={{
+								marginBottom: 10,
+								backgroundColor: btnColor,
+								borderRadius: 12,
+								padding: 12,
+								elevation: 6,
+							}}>
+							{!loading ? (
+								<Text
+									style={{
+										textAlign: 'center',
+										fontSize: 17,
+										color: 'white',
+										fontFamily: 'Lato_400Regular',
+										letterSpacing: 2,
+									}}>
+									{item.status === 3
+										? 'Accept Request'
+										: item.status === 2
+										? 'Update Status'
+										: btn}
+								</Text>
+							) : (
+								<Text
+									style={{
+										textAlign: 'center',
+										fontSize: 13,
+										color: 'white',
+										fontFamily: 'Lato_400Regular',
+										letterSpacing: 2,
+									}}>
+									{item.status === 2 ? 'Updating...' : 'Accepting...'}
+								</Text>
+							)}
+						</View>
+					</TouchableOpacity>
+				</View>
+			</View>
+		</View>
+	);
 };
 
 export default React.memo(Card);
--- store/reducers/requestReducer.js
+++ store/reducers/requestReducer.js
@@ -1,40 +1,35 @@
-import { STORE_REQUESTS, CLEAR_REQUESTS } from '../constants';
+import {STORE_REQUESTS, CLEAR_REQUESTS} from '../constants';
 
-const initialState = { pending: [], ready: [], issued: [] };
+const initialState = {pending: [], ready: [], issued: []};
 
 const requestReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case STORE_REQUESTS:
-      const pending = [];
-      const ready = [];
-      const issued = [];
-      console.log('This is the whole request array', action.payload);
-      console.log('I am pending before foreach', pending);
-      action.payload.forEach(request => {
-        switch (request.status) {
-          case 3:
-            pending.push(request);
-            break;
+	switch (action.type) {
+		case STORE_REQUESTS:
+			const pending = [];
+			const ready = [];
+			const issued = [];
+			action.payload.forEach(request => {
+				switch (request.status) {
+					case 3:
+						pending.push(request);
+						break;
 
-          case 2:
-            ready.push(request);
-            break;
+					case 2:
+						ready.push(request);
+						break;
 
-          case 1:
-            issued.push(request);
-            break;
-        }
-      });
+					case 1:
+						issued.push(request);
+						break;
+				}
+			});
+			return {pending, ready, issued};
+		case CLEAR_REQUESTS:
+			return initialState;
 
-      console.log('I am final pending array', pending);
-      console.log('I am final ready array', ready);
-      return { pending, ready, issued };
-    case CLEAR_REQUESTS:
-      return initialState;
-
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 };
 
 export default requestReducer;
