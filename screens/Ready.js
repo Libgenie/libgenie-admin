8a62c68b5135f90c05b4e2ee5785ed8fa0ed3bd8
saby22 <sabyasachi.tffs@gmail.com>
2021-05-12 21:43:16 +0530

    Removed isLoading state for Pending and Ready

--- screens/Ready.js
+++ screens/Ready.js
@@ -1,62 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, Alert, ActivityIndicator, ImageBackground, FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import {
+	Text,
+	View,
+	Alert,
+	ActivityIndicator,
+	ImageBackground,
+	FlatList,
+} from 'react-native';
+import {useSelector} from 'react-redux';
 import styles from './pendingstyles';
 import Card from '../components/Card';
 import NoData from '../assets/defaultReady';
 
 const Ready = () => {
-  const ready = useSelector(state => state.requests.ready);
+	const ready = useSelector(state => state.requests.ready);
+	return (
+		<ImageBackground
+			source={require('../assets/ready-bg.png')}
+			style={styles.screen}>
+			<View style={styles.headerContainer}>
+				<Text style={styles.header}>Ready</Text>
+			</View>
 
-  if (isLoading)
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <ActivityIndicator size='large' color='#00B7FF' />
-      </View>
-    );
-
-  return (
-    <ImageBackground source={require('../assets/ready-bg.png')} style={styles.screen}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.header}>Ready</Text>
-      </View>
-
-      <View style={styles.cardContainer}>
-        {/* Book Details */}
-        {ready.length > 0 ? (
-          <FlatList
-            data={ready}
-            keyExtractor={readyItem => readyItem.request_id}
-            renderItem={({ item }) => <Card item={item} />}
-          />
-        ) : (
-          <View
-            style={{
-              alignItems: 'center',
-              justifyContent: 'center',
-              flex: 1,
-              marginTop: 100,
-            }}>
-            <NoData />
-            <Text
-              style={{
-                marginTop: 20,
-                fontSize: 15,
-                fontFamily: 'Lato_400Regular',
-                opacity: 0.5,
-              }}>
-              There aren't any books ready for pickup...
-            </Text>
-          </View>
-        )}
-      </View>
-    </ImageBackground>
-  );
+			<View style={styles.cardContainer}>
+				{/* Book Details */}
+				{ready.length > 0 ? (
+					<FlatList
+						data={ready}
+						keyExtractor={readyItem => readyItem.request_id}
+						renderItem={({item}) => <Card item={item} />}
+					/>
+				) : (
+					<View
+						style={{
+							alignItems: 'center',
+							justifyContent: 'center',
+							flex: 1,
+							marginTop: 100,
+						}}>
+						<NoData />
+						<Text
+							style={{
+								marginTop: 20,
+								fontSize: 15,
+								fontFamily: 'Lato_400Regular',
+								opacity: 0.5,
+							}}>
+							There aren't any books ready for pickup...
+						</Text>
+					</View>
+				)}
+			</View>
+		</ImageBackground>
+	);
 };
 
 export default Ready;
